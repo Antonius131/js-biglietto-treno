83a7810c0cd909ab75defa02d3fc51e19d6cd5eb
Antonius131 <antomendetta@gmail.com>
2022-06-14 15:47:23 +0200

    total price calculation

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const tripLenght = parseInt(prompt('Quanti Km vuoi percorrere?'));
+const userAge = parseInt(prompt('Inserisci la tua età'));
+let ticketPrice = tripLenght * 0.21;
+
+if (userAge < 18) {
+   ticketPrice = ticketPrice - ((ticketPrice * 20) / 100);
+}
+
+if (userAge > 65) {
+   ticketPrice = ticketPrice - ((ticketPrice * 40) / 100);
+}
+
+
+const roundedTicketPrice = ticketPrice.toFixed(2);
+console.log (roundedTicketPrice);
